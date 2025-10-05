18d8f979b66487af46719312870ddb2d2d683307
vennhuu <phuochuu0512@gmail.com>
2025-10-05 16:44:05 +0700

    components

--- src/views/App.js
+++ src/views/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.scss';
+import MyComponent from './Example/MyComponent';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <MyComponent />
         </p>
         <a
           className="App-link"
---
+++ src/views/Example/MyComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class MyComponent extends React.Component {
+    render() {
+        return (
+            <div>Hello Phước from QB</div>
+        )
+    }
+}
+
+export default MyComponent;
